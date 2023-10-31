2015a87d08361ef91922836064728923efc17185
Rudra Singh <52309515+RudraSingh4@users.noreply.github.com>
2023-11-01 00:11:19 +0530

    first commit

--- Test-monitor/backend.tf
+++ Test-monitor/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "workflow-bucket-synthetic-monitor"
-    key    = "terraform.tfstate"
+    key    = "test_monitor.tfstate"
     region = "us-east-1"
   }
 }
--- Test2-Monitor/backend.tf
+++ Test2-Monitor/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "workflow-bucket-synthetic-monitor"
-    key    = "terraform.tfstate"
+    key    = "test2_Monitor.tfstate"
     region = "us-east-1"
   }
 }
