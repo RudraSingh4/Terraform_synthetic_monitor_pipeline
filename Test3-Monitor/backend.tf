67e351e31537c72b1fd70495d90b66b43e87d6e4
Rudra Singh <52309515+RudraSingh4@users.noreply.github.com>
2023-10-31 22:56:51 +0530

    first commit

--- Test3-Monitor/backend.tf
+++ Test3-Monitor/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "test-bucket-00006"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "workflow-bucket-synthetic-monitor"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- Test3-Monitor/variable.tf
+++ Test3-Monitor/variable.tf
@@ -1,4 +1,4 @@
 variable "monitor_count3"{
     type = any
-    default = 2
+    default = 25
 }
